3042d7054b46c83f8ed13bccf4c4b1ff24bc6778
PraveenAchari <praveen.achari@accionlabs.com>
2023-03-21 22:57:21 +0530

    commit3

--- EmployeeManagentWebApplication/Controllers/EmployeeController.cs
+++ EmployeeManagentWebApplication/Controllers/EmployeeController.cs
@@ -55,26 +55,6 @@ namespace EmployeeManagentWebApplication.Controllers
             var employee1 = _employee.UpdateEmployee(employee, id);
             return Ok(employee1);
         }
-        [HttpPost]
-        [Route("AddProject")]
-        public IActionResult AddProject(ProjectModel project)
-        {
-            var projects=_employee.CreateProjects(project);
-            return Ok(projects);
-        }
-        [HttpPut]
-        [Route("UpdateProject")]
-        public IActionResult UpdateProject(ProjectModel project, int id)
-        {
-            var project1 = _employee.UpdateProject(project, id);
-            return Ok(project1);
-        }
-        [HttpPost]
-        [Route("AddProjects")]
-        public IActionResult AddProjects(ProjectResourceMappingModel projects)
-        {
-            var project1=_employee.CreateProjectResourceMapping(projects);
-            return Ok(project1);    
-        }
+      
     }
 }
---
+++ EmployeeManagentWebApplication/Controllers/ProjectController.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeManagentWebApplication.Models;
+using EmployeeManagentWebApplication.ServiceLayer.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagentWebApplication.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly IEmployeeService _employee;
+        public ProjectController(IEmployeeService employee)
+        {
+            _employee = employee;
+        }
+        [HttpPost]
+        [Route("AddProject")]
+        public IActionResult AddProject(ProjectModel project)
+        {
+            var projects = _employee.CreateProjects(project);
+            return Ok(projects);
+        }
+        [HttpPut]
+        [Route("UpdateProject")]
+        public IActionResult UpdateProject(ProjectModel project, int id)
+        {
+            var project1 = _employee.UpdateProject(project, id);
+            return Ok(project1);
+        }
+        [HttpPost]
+        [Route("AddProjects")]
+        public IActionResult AddProjects(ProjectResourceMappingModel projects)
+        {
+            var project1 = _employee.CreateProjectResourceMapping(projects);
+            return Ok(project1);
+        }
+    }
+}
